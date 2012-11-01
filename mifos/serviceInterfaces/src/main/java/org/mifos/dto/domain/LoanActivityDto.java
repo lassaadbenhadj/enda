b4d3e6bd48bd03805b721b1e5d5e710b039aaa3b
lassaad <lbhadj@gmail.com>
2012-11-01 14:59:03 +0100

    Add Diplay Recent activity2

--- mifos/serviceInterfaces/src/main/java/org/mifos/dto/domain/LoanActivityDto.java
+++ mifos/serviceInterfaces/src/main/java/org/mifos/dto/domain/LoanActivityDto.java
@@ -24,6 +24,9 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Locale;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @SuppressWarnings("PMD")
 @edu.umd.cs.findbugs.annotations.SuppressWarnings(value={"SE_NO_SERIALVERSIONID", "EI_EXPOSE_REP", "EI_EXPOSE_REP2"}, justification="should disable at filter level and also for pmd - not important for us")
 public class LoanActivityDto implements Serializable {
@@ -44,7 +47,7 @@ public class LoanActivityDto implements Serializable {
     private java.sql.Timestamp timeStamp;
     private String runningBalancePrincipleWithInterestAndFees;
     private double totalValue;
-//    private String userPrefferedDate;
+    private String userPrefferedDate;
 
     public LoanActivityDto() {
     }
@@ -161,10 +164,12 @@ public class LoanActivityDto implements Serializable {
         this.runningBalancePenalty = runningBalancePenalty;
     }
 
-//    public String getUserPrefferedDate() {
-//        return userPrefferedDate;
+    public String getUserPrefferedDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
+        userPrefferedDate= dateFormat.format(getActionDate());
+        return userPrefferedDate;
 //        return DateUtils.getUserLocaleDate(getLocale(), getActionDate().toString());
-//    }
+    }
 
 //    public void setUserPrefferedDate(String userPrefferedDate) {
 //        this.userPrefferedDate = userPrefferedDate;
